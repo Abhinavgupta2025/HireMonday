f7347a202ff65092d6832080a9984bec18735072
Abhinav Gupta <abhinavgupta555251@gmail.com>
2026-04-18 23:23:20 +0530

    middleware updated

--- Backend/MiddleWare/isAuthenticated.js
+++ Backend/MiddleWare/isAuthenticated.js
@@ -12,7 +12,7 @@ const isAuthenticated = async (req, res, next) => {
         const decode = await jwt.verify(token, process.env.SECRET_KEY);
         if(!decode){
             return res.status(401).json({
-                message:"Invalid token",
+                message:"Invalid token id",
                 success:false
             })
         };

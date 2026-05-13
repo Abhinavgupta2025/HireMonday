58e2788814fa8d2d0012d35e917ea0cc825f8441
Abhinav Gupta <abhinavgupta555251@gmail.com>
2026-05-13 21:03:19 +0530

    Fix backend dotenv loading for Backend/.env before routes load

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
+import "./config.js";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import bodyParser from "body-parser";
-import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
 import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
@@ -12,8 +12,6 @@ import notificationRoutes from "./routes/notification.routes.js";
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from '@google/generative-ai';
 import session from 'express-session';
 import passport from 'passport';
-
-dotenv.config({});
 
 const app = express();
 

265cb04bbbfd50ed25977450f3704d262bb6ae98
Abhinav Gupta <abhinavgupta555251@gmail.com>
2026-05-26 18:41:32 +0530

    Update cookie settings for cross-origin production requests

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -138,8 +138,8 @@ export const login = async (req, res) => {
             .cookie("token", token, {
                 maxAge: 1 * 24 * 60 * 60 * 1000,
                 httpOnly: true,
-                sameSite: "lax",
-                secure: process.env.NODE_ENV === "production",
+                sameSite: "none",
+                secure: true,
             })
             .json({
                 message: `Welcome back ${user.fullname}`,
@@ -162,7 +162,7 @@ export const logout = async (req, res) => {
     try {
         return res
             .status(200)
-            .cookie("token", "", { maxAge: 0 })
+            .cookie("token", "", { maxAge: 0, sameSite: "none", secure: true })
             .json({
                 message: "Logged out successfully.",
                 success: true,
